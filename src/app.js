2028265a65c2d60bb40e60305baab804001b6a23
zephyrus21 <pandey.piyus@gmail.com>
2021-04-01 17:44:15 +0530

    add

--- src/app.js
+++ src/app.js
@@ -39,3 +39,32 @@ app.get('/help', (req, res) => {
         name: 'Piyush Pandey'
     })
 })
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Help article not found.'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Page not found.'
+    })
+})
